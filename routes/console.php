60bb2fe09d0f899334f2e6d4f347542cc3a5fc33
Patrick Herrera <patrickfherrera@gmail.com>
2025-12-14 19:47:46 +0800

    Removed the migrate fresh

--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,6 @@
 use App\Models\CoinArbitrage;
 use Illuminate\Support\Facades\Schedule;
 
-// Reset demo environment daily at midnight
-Schedule::command('migrate:fresh --seed --force')->dailyAt('00:00');
-
 CoinArbitrage::where('enabled', '=', 1)->each(function ($coin_arbitrage) {
 //            Schedule::command('app:arbitrage-experimental --coin_arbitrage_id=' . $coin_arbitrage->id)->everyMinute()->withoutOverlapping();
     Schedule::command('arbitrage:run --interval=5 --coin_arbitrage_id=' . $coin_arbitrage->id)->everyMinute()->withoutOverlapping();
