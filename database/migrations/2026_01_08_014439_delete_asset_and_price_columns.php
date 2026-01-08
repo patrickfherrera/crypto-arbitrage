a851885fb46c105d4174b8449b4890eadbd00e45
Patrick Herrera <patrickfherrera@gmail.com>
2026-01-08 09:47:04 +0800

    migration for deleting coin_*_from_asset and coin_*_to_asset columns in the coin_arbitrages table.

---
+++ database/migrations/2026_01_08_014439_delete_asset_and_price_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('coin_arbitrages', function (Blueprint $table) {
+            $table->dropColumn('coin_one_from_asset');
+            $table->dropColumn('coin_one_to_asset');
+            $table->dropColumn('coin_two_from_asset');
+            $table->dropColumn('coin_two_to_asset');
+            $table->dropColumn('coin_three_from_asset');
+            $table->dropColumn('coin_three_to_asset');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('coin_arbitrages', function (Blueprint $table) {
+            //
+        });
+    }
+};
